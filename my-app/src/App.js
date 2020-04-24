fb21cfd43ff77aadb589a66f9c984f89bfd37320
Woojin Lee <dev.woojin.lee@gmail.com>
2020-04-24 19:53:25 +0900

    Add new todo item

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,29 +2,60 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './todos.scss';
 
-function TodoItem(todo_input) {
-  const [todo_string, setTodoString] = useState(todo_input);
-  const [isDone, setIsDone] = useState(false);
-
+const TodoItem = ({ todo }) => {
   return (
-    <li onClick={() => setIsDone(!isDone)} className={isDone ? "completed" : ""} >
-    <span><i class="fa fa-trash"></i></span> {todo_string}
+    <li key={todo.key} className={todo.isDone ? "completed" : ""} >
+    <span><i className="fa fa-trash"></i></span> {todo.text}
     </li>
   );
 }
 
+function TodoInput({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleKeyDown = (event) => {
+    if(event.keyCode === 13) {   // press enter
+      addTodo(value);
+      setValue("");
+    }
+  };
+
+  return (
+    <input
+      type="text"
+      placeholder="Add New Todo" 
+      value={value} 
+      onChange={(e) => setValue(e.target.value)} 
+      onKeyDown={handleKeyDown}
+    />
+  );
+}
+
 function App() {
+  const [todos, setTodos] = useState(
+    [ {text: "First item", isDone: false}, 
+      {text: "Second item", isDone: false}, 
+      {text: "Third item", isDone: false} ]
+    );
+  const [newTodoValue, setNewTodoValue] = useState("");
+
+  const addTodo = (todo) => {
+    const newTodos = [...todos, {text: todo, isDone: false}];
+    setTodos(newTodos);
+  };
 
   return (
     <div className="main">
       <div id="container">
-          <h1>To-Do List<i class="fa fa-plus"></i></h1>
-          <input type="text" placeholder="Add New Todo" />
-          
+          <h1>To-Do List<i className="fa fa-plus"></i></h1>
+          <TodoInput addTodo={addTodo} />
           <ul>
-              {TodoItem("First item")}
-              {TodoItem("Second item")}
-              {TodoItem("Third item")}
+              {todos.map((todo, index) => (
+                <TodoItem 
+                  key={index}
+                  todo={todo}
+                />
+              ))}
           </ul>
       </div>
     </div>
