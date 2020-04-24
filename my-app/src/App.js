ad19d5c0b48cf0458cb7fb54667564449b3b8551
Woojin Lee <dev.woojin.lee@gmail.com>
2020-04-24 17:15:37 +0900

    Add checking off todos by clicking

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './todos.scss';
+
+function TodoItem(todo_input) {
+  const [todo_string, setTodoString] = useState(todo_input);
+  const [isDone, setIsDone] = useState(false);
+
+  return (
+    <li onClick={() => setIsDone(!isDone)} className={isDone ? "completed" : ""} >
+    <span><i class="fa fa-trash"></i></span> {todo_string}
+    </li>
+  );
+}
 
 function App() {
+
   return (
     <div className="main">
       <div id="container">
           <h1>To-Do List<i class="fa fa-plus"></i></h1>
-          <input type="text" placeholder="Add New Todo"/>
+          <input type="text" placeholder="Add New Todo" />
           
           <ul>
-              <li><span><i class="fa fa-trash"></i></span> Go To Potions Class</li>
-              <li><span><i class="fa fa-trash"></i></span> Buy New Robes</li>
-              <li><span><i class="fa fa-trash"></i></span> Visit Hagrid</li>
+              {TodoItem("First item")}
+              {TodoItem("Second item")}
+              {TodoItem("Third item")}
           </ul>
       </div>
     </div>
