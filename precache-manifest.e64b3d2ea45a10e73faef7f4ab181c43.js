4d9ad3c569a5fa489fdeb9825057e6d9e66caeed
Woojin Lee <dev.woojin.lee@gmail.com>
2020-04-27 22:14:36 +0900

    Updates

---
+++ precache-manifest.e64b3d2ea45a10e73faef7f4ab181c43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4ec68e1441b79b9cdfd93d036b52c8",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "fea90cc81a302bf83ba0",
+    "url": "/react-to-do-list/static/css/main.df5b7186.chunk.css"
+  },
+  {
+    "revision": "88781d06648869cd5776",
+    "url": "/react-to-do-list/static/js/2.1ced1e6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do-list/static/js/2.1ced1e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fea90cc81a302bf83ba0",
+    "url": "/react-to-do-list/static/js/main.818a16e8.chunk.js"
+  },
+  {
+    "revision": "2325766b51dc203d03a0",
+    "url": "/react-to-do-list/static/js/runtime-main.aeaaad18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-to-do-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
